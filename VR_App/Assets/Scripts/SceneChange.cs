3a3156d9bf381af53a1afb20d2d9707a507b0be8
FabiRoe <fabian.roedder@gmx.de>
2021-05-30 20:03:25 +0200

    Add MainMenu

---
+++ VR_App/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChange : MonoBehaviour
+{
+
+    public string levelToLoad;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+}
